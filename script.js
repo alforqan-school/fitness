82f7029abad020df70bdd495087c57e869bad92e
alforqan-school <alforqan.school.web@gmail.com>
2025-05-19 20:39:16 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function show(){
-    var x = document.getElementById("topnav");
-    if (x.className === "top-nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "top-nav";
-    }
-}
