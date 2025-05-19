7b8eaa8902f9012a5463122f26cac7dedc96c808
alforqan-school <alforqan.school.web@gmail.com>
2025-05-19 20:38:26 +0300

    Delete coach.js

--- coach.js
+++
@@ -1,8 +0,0 @@
-function selectCoach(selectedCard) {
-    // Remove 'selected' from all cards
-    document.querySelectorAll('.cube-card').forEach(card => card.classList.remove('selected'));
-  
-    // Add 'selected' to the clicked card
-    selectedCard.classList.add('selected');
-  }
-  
